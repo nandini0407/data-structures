6e8af8aec0febeff778b4d7b6eaeba045f4a57a7
nandini0407 <nandini0407@gmail.com>
2017-03-06 16:13:29 -0800

    add delete_min to binary search tree class

--- trees/binary_search_tree.rb
+++ trees/binary_search_tree.rb
@@ -15,10 +15,53 @@ class BinarySearchTree
     @root = root
   end
 
-  def insert
+  def insert(value)
+    BinarySearchTree.insert!(@root, value)
+  end
+
+  def find(value)
+    BinarySearchTree.find!(@root, value)
   end
 
   def delete
+  end
+
+  private
+
+  def self.insert!(node, value)
+    if node.nil?
+      return BSTNode.new(value)
+    else
+      if value <= node.value
+        node.left_child = BinarySearchTree.insert!(node.left_child, value)
+      else
+        node.right_child = BinarySearchTree.insert!(node.right_child, value)
+      end
+      return node
+    end
+  end
+
+  def self.find!(node, value)
+    if node.nil?
+      return nil
+    elsif value == node.value
+      return node
+    elsif value < node.value
+      return BinarySearchTree.find!(node.left_child, value)
+    elsif value > node.value
+      return BinarySearchTree.find!(node.right_child, value)
+    end
+  end
+
+  def self.delete_min!(node)
+    if node.left_child.nil?
+      node_temp = node.right_child
+      node.right_child = nil
+      node = node_temp
+      return node
+    else
+      return BinarySearchTree.delete_min!(node.left_child)
+    end
   end
 
 end
--- trees/tree_exercises.rb
+++ trees/tree_exercises.rb
@@ -1,4 +1,5 @@
 require_relative 'binary_tree'
+require_relative 'binary_search_tree'
 
 def build_binary_tree(arr)
   bt_arr = []
@@ -17,3 +18,14 @@ def build_binary_tree(arr)
 end
 
 print build_binary_tree([1, 2, 3, 4, 5, 6])
+
+def build_binary_search_tree(arr)
+  bst_arr = []
+  arr.each_with_index do |el, i|
+    if (i - 1) / 2 >= 0
+    else
+      root = BSTNode.new(el)
+      bst_arr << BinarySearchTree.new(root)
+    end
+  end
+end
