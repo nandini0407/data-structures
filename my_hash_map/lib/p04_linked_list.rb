0b8e4737f345d371043dfcd0d14d1faeeb4b26f5
nandini0407 <nandini0407@gmail.com>
2017-03-09 15:51:55 -0800

    implement hash using linked list

--- my_hash_map/lib/p04_linked_list.rb
+++ my_hash_map/lib/p04_linked_list.rb
@@ -15,11 +15,21 @@ class Link
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+    @prev = nil
+    @next = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +38,68 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    each do |link|
+      return link.val if link.key == key
+    end
+    nil
   end
 
   def include?(key)
+    each do |link|
+      return true if link.key == key
+    end
+    false
   end
 
   def append(key, val)
+    next_key = Link.new(key, val)
+    next_key.prev = @tail.prev
+    next_key.next = @tail
+    @tail.prev.next = next_key
+    @tail.prev = next_key
   end
 
   def update(key, val)
+    each do |link|
+      if link.key == key
+        link.val = val
+      else
+        append(key, val)
+      end
+    end
   end
 
   def remove(key)
+    each do |link|
+      if link.key == key
+        link.prev.next = link.next
+        link.next.prev = link.prev
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    link = first
+    until link == @tail
+      prc.call(link)
+      link = link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
