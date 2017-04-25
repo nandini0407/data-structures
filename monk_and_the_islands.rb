8ca060009773269b252291d80c5c6df3cf5d4f93
nandini0407 <nandini0407@gmail.com>
2017-04-24 22:20:50 -0700

    add monk and islands problem to data structures

---
+++ monk_and_the_islands.rb
@@ -0,0 +1,34 @@
+# Monk visits the land of Islands. There are a total of N islands numbered from 1 to N. Some pairs of
+# islands are connected to each other by Bidirectional bridges running over water. Monk hates to cross
+# these bridges as they require a lot of effort. He is standing at Island #1 and wants to reach the
+# Island #N. Find the minimum the number of bridges that he shall have to cross, if he takes the optimal route.
+
+def monk_and_islands(islands, n) # islands is the graph that denotes bridge connections between islands
+  queue = []
+  visited = Hash.new
+  distances = Hash.new
+
+  queue << 1
+  distances[1] = 0
+
+  until queue.empty?
+    current = queue.shift
+    visited[current] = true
+
+    islands[current].each do |neighbor|
+      unless visited[neighbor]
+        queue << neighbor
+        if distances[neighbor]
+          distances[neighbor] = [(distances[current] + 1), distances[neighbor]].min
+        else
+          distances[neighbor] = distances[current] + 1
+        end
+      end
+    end
+  end
+
+  distances[n]
+end
+
+puts monk_and_islands({ 1 => [2], 2 => [3], 3 => [] }, 3)
+puts monk_and_islands({ 1 => [2], 2 => [3, 4], 3 => [4], 4 => [2] }, 4)
